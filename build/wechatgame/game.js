2d104d156f2b3777a98aaadb8c8ba46cc16abc28
bird_wangkun <you@example.com>
2018-09-17 15:44:38 +0800

    删除本地文件

--- build/wechatgame/game.js
+++
@@ -1,8 +0,0 @@
-require('libs/weapp-adapter/index');
-var Parser = require('libs/xmldom/dom-parser');
-window.DOMParser = Parser.DOMParser;
-require('libs/wx-downloader.js');
-wxDownloader.REMOTE_SERVER_ROOT = "undefined";
-wxDownloader.SUBCONTEXT_ROOT = "";
-require('src/settings');
-require('main');
